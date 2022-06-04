26b9e2badc57fd4cb287fe23cbd6e390509279df
martinpuga1 <martinpuga500@gmail.com>
2022-06-04 13:29:23 -0500

    Proyecto Integrador

--- js/index.js
+++ js/index.js
@@ -3,3 +3,29 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.modal').modal();
 });
+var nombre = document.getElementById('correo');
+var password = document.getElementById('contraseña');
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+function enviarFormulario()
+{
+    console.log('Enviando formulario..');
+    var mensajesError=[];
+
+    if(nombre.value === null || nombre.value === '')
+    {
+        mensajesError.push('Ingresa tu correo electronico');
+    }
+    if(password.value === null || password.value === '')
+    {
+        mensajesError.push('Ingresa tu password');
+    }
+
+   error.innerHTML =  mensajesError.join(',');
+    return false;
+}
+function alerta()
+{
+    alert("esta sección se encuentra en desarrollo");
+}
